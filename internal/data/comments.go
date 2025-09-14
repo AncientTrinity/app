cbbf41c7efb9bfef9db22a3b9744475ffc77f1d3
Victor A. Tillett <victillettjr@gmail.com>
2025-09-13 23:15:11 -0600

    Added Update and  Delete Completing the CRUD part

--- internal/data/comments.go
+++ internal/data/comments.go
@@ -11,6 +11,7 @@ import (
 
 var (
 	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
 )
 
 type Comment struct {
@@ -64,6 +65,50 @@ func (c CommentModel) Get(id int64) (*Comment, error) {
 		}
 	}
 	return &comment, nil
+}
+
+func (c CommentModel) Update(comment *Comment) error {
+	query := `
+		UPDATE comments
+		SET content = $1, author = $2, version = version + 1
+		WHERE id = $3 AND version = $4
+		RETURNING version`
+	args := []any{
+		comment.Content,
+		comment.Author,
+		comment.ID,
+		comment.Version,
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	err := c.DB.QueryRowContext(ctx, query, args...).Scan(&comment.Version)
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return ErrEditConflict
+		default:
+			return err
+		}
+	}
+	return nil
+}
+
+func (c CommentModel) Delete(id int64) error {
+	query := `DELETE FROM comments WHERE id = $1`
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	result, err := c.DB.ExecContext(ctx, query, id)
+	if err != nil {
+		return err
+	}
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
+	return nil
 }
 
 func ValidateComment(v *validator.Validator, comment *Comment) {
